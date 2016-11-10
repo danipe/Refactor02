9feaa3166a55d8b4aa742ddfd08b0bf92a3fddf4
daniel@example.com <daniel@example.com>
2016-11-10 20:22:25 +0100

    Refactor02

--- src/Refactor02.java
+++ src/Refactor02.java
@@ -12,31 +12,23 @@ public class Refactor02 {
 		int[] jugador1={2,5,3,0,1};
 		int[] jugador2={1,1,3,1,4};
 		int[] jugador3={1,0,2,1,0};
-		float resultado=0;
+		
 
 		//Calculamos la media geométrica para el primer jugador
-		for(int i=0;i<jugador1.length;i++)
-		{
-			resultado=resultado+jugador1[i];
-		}
-		resultado=resultado/jugador1.length;
-		System.out.println("La media de goles del jugador 1 es: "+resultado);
-		resultado=0;
+		calculaMedia(jugador1,"1");
 		//Calculamos la media geométrica para el segundo jugador
-		for(int i=0;i<jugador2.length;i++)
-		{
-			resultado=resultado+jugador2[i];
-		}
-		resultado=resultado/jugador2.length;
-		System.out.println("La media de goles del jugador 2 es: "+resultado);
-		resultado=0;
+		calculaMedia(jugador2,"2");
 		//Calculamos la media geométrica para el tercer jugador
-		for(int i=0;i<jugador3.length;i++)
+		calculaMedia(jugador3,"3");
+	}
+	public static void calculaMedia(int[] jugador, String sjug){
+		float resultado=0;
+		for(int i=0;i<jugador.length;i++)
 		{
-			resultado=resultado+jugador3[i];
+			resultado=resultado+jugador[i];
 		}
-		resultado=resultado/jugador3.length;
-		System.out.println("La media de goles del jugador 3 es: "+resultado);
+		resultado=resultado/jugador.length;
+		System.out.println("La media de goles del jugador "+sjug+" es: "+resultado);
 		resultado=0;
 	}
 
